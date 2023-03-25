e45e2863bbb0a45e843a4c13b9741e3cb3421837
Felix Vik <felixhusbanan@gmail.com>
2023-03-25 22:45:04 +0100

    sigma

--- terning.js
+++ terning.js
@@ -4,11 +4,56 @@ let straffeBtn = document.querySelector('#straffekast')
 let timerBtn = document.querySelector('#timer')
 let comEl = document.querySelector('#com')
 let bodyEl = document.getElementsByTagName('body')[0]
-let stressAu = document.querySelector('#stress')
-let duckAu = document.querySelector('#duck')
+let stressAu = document.querySelector('#stressAu')
+let ferdigAu = document.querySelector('#ferdigAu')
+let situasjonAu = document.querySelector('#situasjonAu')
 let reglerBtn = document.querySelector('#regler')
 let reglerEl = document.querySelector('#reglerTxt')
-let carhornAu = document.querySelector('#carhorn')
+let tempoEls = document.querySelectorAll('.tempo button')
+let lyderEls = document.querySelectorAll('.lyder button')
+let audioEls = document.querySelectorAll('.audios audio')
+
+
+let vanligArr = ["stressmusikk.mp3", "duck.mp3", "carhorn.mp3"]
+let sigmaArr = ["heartSong.mp3", "sigmaSound.mp3", "tiger.mp3"]
+
+let tempo = 1
+tempoEls[1].classList.add('checked')
+for(let i=0; i<tempoEls.length; i++){
+    tempoEls[i].addEventListener('click', function(){
+        for(let j=0; j<tempoEls.length; j++){
+            tempoEls[j].classList.remove('checked')
+        }
+        tempoEls[i].classList.add('checked')
+        tempo = i
+    })
+}
+tempoEls[1].classList.add('checked')
+
+lyderEls[0].classList.add('checked')
+lyderEls[1].classList.remove('checked')
+lyderEls[0].onclick = function(){
+    lyderEls[0].classList.add('checked')
+    lyderEls[1].classList.remove('checked')
+    for(let i=0; i<audioEls.length; i++){
+        console.log(`${audioEls[i].src} = ./terninger/lyder/${vanligArr[i]}`)
+        audioEls[i].src = `./terninger/lyder/${vanligArr[i]}`
+    }
+}
+lyderEls[1].onclick = function(){
+    console.log("sigma aktivert")
+    lyderEls[1].classList.add('checked')
+    lyderEls[0].classList.remove('checked')
+    for(let i=0; i<audioEls.length; i++){
+        console.log(`${audioEls[i].src} = ./terninger/lyder/${sigmaArr[i]}`)
+        audioEls[i].src = `./terninger/lyder/${sigmaArr[i]}`
+    }
+}
+
+terningEl.addEventListener('click', kast)
+redemptionBtn.addEventListener('click', redemption)
+timerBtn.addEventListener('click', timer)
+straffeBtn.addEventListener('click', straffekast)
 
 reglerBtn.onclick = function(){
     console.log("regler klikk")
@@ -21,136 +66,128 @@ function sleep(ms) {
 // Liste over terningbildene
 let terninger = ['en2', 'to2', 'tre2', 'fire2', 'fem2', 'seks2']
 
-// Forskjellige situasjoner
-let situasjonerArr = ["Elias tok deg i bakken! vent 3 sek", "Johannes putta deg i fryseren! vent 5 sek", "Andreas viser deg en baddie fra insta! Vent 2 sek", "Terningen falt under bordet! vent 3 sek", "Elias er ikke enig i reglene! Vent 1 sek", "Filip snakker om IKEA! Vent 2 sek", "Elias dro og spilte piano! Vent 2 sek", "Thorvald tente på skoene dine! Vent 6 sek", "Felix sa noe morsomt og du fikk latterkrampe! Vent 1 sek", "Johannes rapper en hel sang! Vent 5 sekunder", "Felix trykker på HDMI out! Dere er DOOMED. Vent 8 sekunder"]
-let situasjonTid = [3, 5, 2, 3, 1, 2, 2, 6, 1, 5, 8]
+let situasjonerArr
+fetch("situasjoner.json")
+    .then(res => res.json())
+    .then(situasjon => {
+        situasjonerArr = situasjon
+})
 
 let terningNr = Math.floor(Math.random()*6)
 terningEl.src = `./terninger/${terninger[terningNr]}.png`
-// Kaster terningen
-function kaster(){
-    terningNr = Math.floor(Math.random()*6)
-    terningEl.src = `./terninger/${terninger[terningNr]}.png`
+
+async function situasjon(){
+    // Bestemmer et tilfeldig tall
+    let situasjonNr = Math.floor(Math.random()*(situasjonerArr.length/* *10 */))
+    // Hvis tilfeldig tall er mindre enn antall situasjoner, situasjonen popper opp
+    if (situasjonNr < situasjonerArr.length){
+        situasjonAu.play()
+        comEl.innerHTML = situasjonerArr[situasjonNr].situasjon
+        terningEl.removeEventListener('click', kast)
+        console.log("situasjontid = " + situasjonerArr[situasjonNr].tid)
+        await sleep(situasjonerArr[situasjonNr].tid*1000)
+        console.log("klar til kast")
+        terningEl.addEventListener('click', kast)
+    }
 }
 
-// Mulig å kaste førstegang
-let kastMulig = true
-async function kast(){
-    // Hvis terningen ikke triller og timeren er på vil den kaste
-    if (kastMulig == true && timerOn == true){
-        comEl.innerHTML = `Kast 6 eller 1`
-        // Ikke mulig å kaste imens
-        kastMulig = false
-        // Rullerer gjennom terninger imens kast
-        for(i=50;i<250;i=i+50){
-            kaster()
-            terningEl.classList.toggle('rist')
-            await sleep(i)
-        }
+// Kaster terningen
+async function kaster(){
+    terningEl.removeEventListener('click', kast)
+    // Rullerer gjennom terninger imens kast
+    for(i=50; i<250; i=i+50){
+        let midlTerningNr = Math.floor(Math.random()*6)
+        terningEl.src = `./terninger/${terninger[midlTerningNr]}.png`
+        terningEl.classList.toggle('rist')
+        terningNr = midlTerningNr
+        await sleep(i)
+    }
 
-        // Bestemmer et tilfeldig tall
-        let situasjon = Math.floor(Math.random()*135)
-        // Hvis tilfeldig tall er mindre enn antall situasjoner, situasjonen popper opp
-        if (situasjon < 11 && timerOn == true){
-            carhornAu.play()
-            comEl.innerHTML = situasjonerArr[situasjon]
-            kastMulig = false
-            console.log(situasjonTid[situasjon])
-            await sleep(situasjonTid[situasjon]*1000)
-            kastMulig = true
-        }
-        // Hvis triller 1 eller 6 står det "neste person"
-        if(terningNr == 0 || terningNr == 5){
-            console.log('6 eller 1')
-            console.log(straffeOn)
-            if (straffeOn == false){
-                comEl.innerHTML = `Neste person`
-            }
-        }
-        // Mulig å kaste igjen
-        kastMulig = true
-}}
-let straffesum
-// Timer er ikke på
-let timerOn = false
+    // Hvis triller 1 eller 6 står det "neste person"
+    if(terningNr == 0 || terningNr == 5){
+        comEl.innerHTML = `Neste person!`
+    }
+    terningEl.addEventListener('click', kast)
+}
+async function kast(){
+    if (canRoll == true){
+        console.log("kaster nu")
+        // Hvis timeren er på vil den kaste
+        comEl.innerHTML = `Kast 6 eller 1`
+        await kaster()
+        situasjon()
+    }
+
+}
+
+let canRoll = false
 async function timer(){
+    canRoll = true
     stressAu.currentTime = 0
     stressAu.play()
     straffesum = 0
     // Ikke mulig å skru på timer
     timerBtn.removeEventListener('click', timer)
+    timerBtn.innerHTML = "kast!"
     // Mulig å kaste
-    timerOn = true
     bodyEl.style.backgroundColor = "rgb(175, 75, 75)"
     comEl.innerHTML = `Kast 6 eller 1`
     // Setter tilfeldig tid
-    let tid = Math.floor(Math.random()*60)
+    let tid
+    if(tempo == 0){
+        tid = Math.floor(Math.random()*30)+30
+    }else if(tempo == 1){
+        tid = /* Math.floor(Math.random()*30)+15 */10
+    }else if(tempo == 2){
+        tid = Math.floor(Math.random()*30)
+    }
+    console.log(tid)
     // Timer
     while (tid>0){
-        console.log(tid)
+        /* console.log(tid) */
         await sleep(1000)
         tid--
     }
-    bodyEl.style.backgroundColor = "rgb(57, 114, 137)"
-    // Lov å bruke timer igjen
-    timerOn = false
-    // Ikke 1 eller 6 når tiden er ute
-    if (terningNr > 0 && terningNr < 5){
-        comEl.innerHTML = `Tiden er ute! (taper)`
-    }
+    bodyEl.style.backgroundColor = "rgb(57, 137, 90)"
+
+    comEl.innerHTML = `Tiden er ute!`
+    timerBtn.innerHTML = "start timer"
     timerBtn.addEventListener('click', timer)
     // Lov å bruke redemption
     redemptionBtn.style.display = 'block'
     timerBtn.style.display = 'none'
     stressAu.pause()
-    duckAu.play()
+    ferdigAu.play()
+    canRoll = false
 }
-// Ikke lov med redemption fra før av
+// Redemption kast
 async function redemption(){
-        duckAu.pause()
-        timerOn = true
-        bodyEl.style.backgroundColor = "rgb(57, 137, 90)"
-        kast()
-        await sleep(550)
+        ferdigAu.pause()
+        await kaster()
         console.log(`TerningNr = ${terningNr}`)
         if (terningNr > 0 && terningNr < 5){
-            comEl.innerHTML = `Du tapte`
+            comEl.innerHTML = `Du tapte!`
             straffeBtn.style.display = 'block'
             redemptionBtn.style.display = 'none'
-            timerBtn.style.display = 'none'
+            bodyEl.style.backgroundColor = "rgb(187, 137, 49)"
         }
-        timerOn = false
-
 }
-let straffeOn = false
+let straffesum
 async function straffekast(){
-    bodyEl.style.backgroundColor = "rgb(187, 137, 49)"
     console.log(`straffesum = ${straffesum}`)
-    timerOn = true
-    straffeOn = true
-    kast()
-    await sleep(550)
+    await kaster()
     straffesum += terningNr+1
     if (terningNr == 5){
-        comEl.innerHTML = `Kast igjen`
+        comEl.innerHTML = `Kast igjen!`
     }
     else if (terningNr == 0){
-        comEl.innerHTML = `Du skal downe`
+        comEl.innerHTML = `Du skal downe!`
         straffeBtn.style.display = 'none'
-        redemptionBtn.style.display = 'none'
         timerBtn.style.display = 'block'
     }
     else{
-        comEl.innerHTML = `Du skal ta ${straffesum}`
+        comEl.innerHTML = `Du skal ta ${straffesum}!`
         straffeBtn.style.display = 'none'
-        redemptionBtn.style.display = 'none'
         timerBtn.style.display = 'block'
     }
-    straffeOn = false
-    timerOn = false
 }
-
-redemptionBtn.addEventListener('click', redemption)
-terningEl.addEventListener('click', kast)
-timerBtn.addEventListener('click', timer)
-straffeBtn.addEventListener('click', straffekast)
